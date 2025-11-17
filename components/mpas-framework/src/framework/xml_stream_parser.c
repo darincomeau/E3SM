181790c84ea84b91247f45c98e42b88f21a8b5cd
Anthony Craig <anthony.p.craig@gmail.com>
2025-11-17 10:34:23 -0600

    Initial Barfoot port - using intel classic - netcdf NOT via a module - no pnetcdf
    Add support for "netcdf,cdf5" as an mpas stream io_type option.
    This is netcdf + 64bit_data (large file + large variable).
    This is needed to run the aerosi configurations without pnetcdf.
    Update the mpaso and mpassi stream io_type to automatically set
    "pnetcdf,cdf5" or "netcdf,cdf5" depending on the default pio iotype.

--- components/mpas-framework/src/framework/xml_stream_parser.c
+++ components/mpas-framework/src/framework/xml_stream_parser.c
@@ -1276,6 +1276,11 @@ void xml_stream_parser(char *fname, void *manager, int *mpi_comm, int *status)
 				snprintf(msgbuf, MSGSIZE, "        %-20s%s", "I/O type:", "NetCDF-4/HDF5");
 				mpas_log_write_c(msgbuf, "MPAS_LOG_OUT");
 			}
+			else if (strstr(iotype, "netcdf,cdf5") != NULL) {
+				i_iotype = 5;
+				snprintf(msgbuf, MSGSIZE, "        %-20s%s", "I/O type:", "Serial NetCDF (CDF-5, large variable support)");
+				mpas_log_write_c(msgbuf, "MPAS_LOG_OUT");
+			}
 			else if (strstr(iotype, "netcdf") != NULL) {
 				i_iotype = 2;
 				snprintf(msgbuf, MSGSIZE, "        %-20s%s", "I/O type:", "Serial NetCDF");
@@ -1609,6 +1614,11 @@ void xml_stream_parser(char *fname, void *manager, int *mpi_comm, int *status)
 			else if (strstr(iotype, "netcdf4") != NULL) {
 				i_iotype = 3;
 				snprintf(msgbuf, MSGSIZE, "        %-20s%s", "I/O type:", "NetCDF-4/HDF5");
+				mpas_log_write_c(msgbuf, "MPAS_LOG_OUT");
+			}
+			else if (strstr(iotype, "netcdf,cdf5") != NULL) {
+				i_iotype = 5;
+				snprintf(msgbuf, MSGSIZE, "        %-20s%s", "I/O type:", "Serial NetCDF (CDF-5, large variable support)");
 				mpas_log_write_c(msgbuf, "MPAS_LOG_OUT");
 			}
 			else if (strstr(iotype, "netcdf") != NULL) {
@@ -2010,6 +2020,11 @@ void xml_stream_get_attributes(char *fname, char *streamname, int *mpi_comm, cha
 				else if (strstr(xml_iotype, "netcdf4") != NULL) {
 					sprintf(io_type, "%s", xml_iotype);
 					snprintf(msgbuf, MSGSIZE, "Using io_type NetCDF-4/HDF5 for mesh stream");
+					mpas_log_write_c(msgbuf, "MPAS_LOG_OUT");
+				}
+				else if (strstr(xml_iotype, "netcdf,cdf5") != NULL) {
+					sprintf(io_type, "%s", xml_iotype);
+					snprintf(msgbuf, MSGSIZE, "Using io_type Serial NetCDF (CDF-5, large variable support) for mesh stream");
 					mpas_log_write_c(msgbuf, "MPAS_LOG_OUT");
 				}
 				else if (strstr(xml_iotype, "netcdf") != NULL) {
